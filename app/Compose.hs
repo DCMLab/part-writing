1dac227adc6ed8e559d93013e50f55c41b2bc7cd
Christoph Finkensiep <chfin@chfin.de>
2018-08-18 15:37:13 +0200

    make composition easier

--- app/Compose.hs
+++ app/Compose.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import VoiceLeading.Base
@@ -14,8 +16,10 @@ import VoiceLeading.IO.LilyPond (viewPiece, viewPieceTmp)
 import VoiceLeading.IO.Plotting
 import VoiceLeading.IO.Model (loadModel)
 
-import Options.Applicative
+import Options.Applicative as OA
 import Data.Semigroup ((<>))
+import Data.Yaml as Yaml
+import qualified Data.Text as T
 import Control.Monad (unless, when)
 
 data PieceStart = TestPiece
@@ -38,80 +42,133 @@ data Opts = Opts
   , quiet      :: Bool }
   deriving (Show)
 
-opts :: Parser Opts
-opts = Opts
+defaultOpts = Opts
+  { iterations = 20
+  , startWith = TestPiece
+  , keepVoices = []
+  , fPower = Cnst 5.0
+  , modelFp = "model.json"
+  , outputFp = ""
+  , diagramFp = "diagram.pdf"
+  , featuresFp = "estimate_feats.pdf"
+  , compareCrp = False
+  , comparePfx = ""
+  , quiet = False }
+
+parseRead :: Read a => String -> Yaml.Value -> Yaml.Parser a
+parseRead expected = withText expected $ \v -> pure (read (T.unpack v))
+
+instance FromJSON PieceStart where
+  parseJSON = parseRead "TestPiece | NewPiece INT | CorpusPiece NAME | File FILE"
+
+instance FromJSON ChoralVoice where
+  parseJSON = parseRead "ChoralVoice"
+
+instance FromJSON RFun where
+  parseJSON = parseRead "RFun"
+
+instance FromJSON Opts where
+  parseJSON = withObject "Opts" $ \v -> Opts
+    <$> v .:? "iterations" .!= (iterations defaultOpts)
+    <*> v .:? "start-with" .!= (startWith defaultOpts)
+    <*> v .:? "keep-voices" .!= (keepVoices defaultOpts)
+    <*> v .:? "power" .!= (fPower defaultOpts)
+    <*> v .:? "model" .!= (modelFp defaultOpts)
+    <*> v .:? "output" .!= (outputFp defaultOpts)
+    <*> v .:? "diagram-out" .!= (diagramFp defaultOpts)
+    <*> v .:? "feature-summary-out" .!= (featuresFp defaultOpts)
+    <*> pure False
+    <*> v .:? "compare-corpus-prefix" .!= (comparePfx defaultOpts)
+    <*> pure False
+
+data Conf = Conf { confCompose :: Opts }
+
+instance FromJSON Conf where
+  parseJSON = withObject "Conf" $ \v -> Conf
+    <$> v .:? "compose" .!= defaultOpts
+
+loadConfig :: FilePath -> IO Opts
+loadConfig fp = do
+  conf <- decodeFileEither fp
+  pure $ either (const defaultOpts) confCompose conf
+
+opts :: Opts -> OA.Parser Opts
+opts defs = Opts
   <$> argument auto
   ( help "number of estimation iterations"
     <> showDefault
-    <> value 20
+    <> value (iterations defs)
     <> metavar "iterations" )
   <*> option auto
   ( long "start-with"
     <> short 's'
     <> help "initializes the estimation procedure"
     <> showDefault
-    <> value TestPiece
+    <> value (startWith defs)
     <> metavar "TestPiece | \"NewPiece INT\" | \"CorpusPiece NAME\" | \"File FILE\"")
   <*> option auto
   ( long "keep-voices"
     <> short 'k'
     <> help "keep these voices from the initial piece (comma separated in \"[]\")"
     <> showDefault
-    <> value []
+    <> value (keepVoices defs)
     <> metavar "\"[(Soprano|Alto|Tenor|Bass)*]\"")
   <*> option auto
   ( long "power"
     <> short 'p'
     <> help "the value of the annealing power (over time)"
     <> showDefault
-    <> value (Cnst 5)
+    <> value (fPower defs)
     <> metavar "RFUN")
   <*> strOption
   ( long "model"
     <> short 'm'
     <> help "model file used for estimation"
     <> showDefault
-    <> value "model.json"
+    <> value (modelFp defs)
     <> metavar "FILE" )
   <*> strOption
   ( long "output"
     <> short 'o'
     <> help "LilyPond filename of final composition. If empty, a temporary file will be used."
     <> showDefault
-    <> value ""
+    <> value (outputFp defs)
     <> metavar "FILE" )
   <*> strOption
   ( long "diagram-out"
     <> short 'd'
     <> help "filename for the model diagram"
     <> showDefault
-    <> value "diagram.pdf"
+    <> value (diagramFp defs)
     <> metavar "FILE" )
   <*> strOption
   ( long "feature-summary-out"
     <> short 'f'
     <> help "filename for the feature summary diagram"
     <> showDefault
-    <> value "estimate_feats.pdf"
+    <> value (featuresFp defs)
     <> metavar "FILE" )
   <*> switch
-  ( long "compare-corpus" <> short 'c' <> help "plot a comparison of the piece with corpus data" )
+  ( long "compare-corpus"
+    <> short 'c'
+    <> help "plot a comparison of the piece with corpus data" )
   <*> strOption
   ( long "compare-corpus-prefix"
     <> showDefault
-    <> value ""
+    <> value (comparePfx defs)
     <> help "corpus comparison filenames use the prefix PREFIX"
     <> metavar "PREFIX")
     <*> switch
-  ( long "no-plots" <> short 'q' <> help "don't writey plot files")
+  ( long "no-plots" <> short 'q' <> help "don't write plot files")
 
-optsInfo = info (opts <**> helper)
+optsInfo defs = info (opts defs <**> helper)
   (fullDesc <> progDesc "Compose a piece by MAP estimation")
 
 main :: IO ()
 main = do
+  defs <- loadConfig "defaults.yaml"
   -- read options
-  options <- execParser optsInfo
+  options <- execParser $ optsInfo defs
   putStrLn $ show options
   let q = quiet options
 
---
+++ compose-sets.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+import sys
+
+if __name__ == "__main__":
+    print(sys.argv[1])
