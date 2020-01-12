40ddbe9e8eda9af454084c2070e7e2b8f1c498a5
Christoph Finkensiep <chfin@chfin.de>
2020-01-12 21:01:49 +0100

    better plotting colors

--- src/VoiceLeading/IO/Plotting.hs
+++ src/VoiceLeading/IO/Plotting.hs
@@ -6,9 +6,12 @@ import VoiceLeading.Automaton (NamedFeature(..))
 import Control.Concurrent (forkIO)
 import Control.Monad (zipWithM_)
 import qualified Data.Text as T
+import Control.Lens (use)
 
 import Graphics.Rendering.Chart.Easy
 import Graphics.Rendering.Chart.Backend.Cairo (toFile, FileOptions(..), FileFormat(..))
+import qualified Data.Colour.Palette.ColorSet as K
+import qualified Data.Colour.Palette.BrewerSet as K
 
 import VoiceLeading.IO.HorizontalBars
 
@@ -20,13 +23,13 @@ plotOverFeatures fp title names values = do
     layout_top_axis_visibility .= def
     layout_y_axis . laxis_generate .= autoIndexAxis (reverse $ map T.unpack names)
     layout_y_axis . laxis_style . axis_label_style . font_size .= 8
+    setColors $ opaque . K.d3Colors1 <$> [0..]
     plot $ plotHBars <$> barPlot -- bars ["parameter value"] (addIndexes (map (:[]) ps))
       where --mkParamMap = zipWith (\n p -> (n,[p])) (map nfName nfs) ps
             opts = FileOptions (600,2200) PDF
             barPlot = liftEC $ do
-              color <- takeColor
-              let styles = [(solidFillStyle color, Just (solidLine 0.5 $ opaque black))]
-              plot_bars_item_styles .= styles
+              cols <- liftCState $ use colors
+              plot_bars_item_styles .= [(solidFillStyle c, Nothing) | c <- cols]
               plot_bars_titles .= ["parameter value"]
               plot_bars_values .= (addIndexes (reverse $ map (:[]) values))
               plot_bars_style .= BarsClustered
