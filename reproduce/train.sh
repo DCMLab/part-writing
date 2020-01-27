ee9554f82d3401af6dea1d60f5ac542222114418
Christoph Finkensiep <chfin@chfin.de>
2020-01-27 13:30:20 +0100

    drop -q option

--- reproduce/train.sh
+++ reproduce/train.sh
@@ -2,4 +2,4 @@
 
 stack exec vl-train -- 5000 -s 1 -z 0.1 -c 1 -r 0\
       -l "Lin 0.3 0" -f "Lin 0.1 0.3" -p "Cnst 1"\
-      -o "model.json" -L "train_reproduce.log" -q
+      -o "model.json" -L "train_reproduce.log"
